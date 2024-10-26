dfd4bd0510cdad664dd4c352903450af15cb6e02
JacoWymowny <jacekkombo@gmail.com>
2024-10-26 17:07:39 +0200

    add devtools query

--- client/src/providers/AppProviders.tsx
+++ client/src/providers/AppProviders.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 interface AppProvidersProps {
   children: ReactNode;
@@ -8,7 +9,12 @@ interface AppProvidersProps {
 const queryClient = new QueryClient();
 
 const AppProviders = ({ children }: AppProvidersProps) => {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} position='bottom' />
+    </QueryClientProvider>
+  );
 };
 
 export default AppProviders;
