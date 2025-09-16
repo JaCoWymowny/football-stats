feff544690eef24b612569a90554f715700adf29
JacoWymowny <jacekkombo@gmail.com>
2025-09-16 13:15:30 +0200

    temporally delete  high password verify + change database to postgres

--- client/src/features/auth/components/forms/register/registerSchema.ts
+++ client/src/features/auth/components/forms/register/registerSchema.ts
@@ -14,10 +14,10 @@ export const registerSchema = z
     password: z
       .string()
       .min(8, 'Hasło musi mieć co najmniej 8 znaków')
-      .max(25, 'Hasło nie może mieć więcej niż 25 znaków')
-      .regex(/[A-Z]/, 'Hasło musi zawierać co najmniej jedną wielką literę')
-      .regex(/[0-9]/, 'Hasło musi zawierać co najmniej jedną cyfrę')
-      .regex(/[@$!%*?&]/, 'Hasło musi zawierać co najmniej jeden znak specjalny'),
+      .max(25, 'Hasło nie może mieć więcej niż 25 znaków'),
+    // .regex(/[A-Z]/, 'Hasło musi zawierać co najmniej jedną wielką literę')
+    // .regex(/[0-9]/, 'Hasło musi zawierać co najmniej jedną cyfrę')
+    // .regex(/[@$!%*?&]/, 'Hasło musi zawierać co najmniej jeden znak specjalny'),
     confirmPassword: z.string(),
   })
   .refine(data => data.password === data.confirmPassword, {
